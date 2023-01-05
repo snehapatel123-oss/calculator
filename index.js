277c65bbb73af941ed04f2a94c246bbd86856b80
snehapatel123-oss <116440431+snehapatel123-oss@users.noreply.github.com>
2023-01-05 20:55:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+function clearScreen() {
+    document.getElementById("input").value = "";
+}
+
+function remove(){
+    var p = document.getElementById("input").value;
+    var q = p.slice(0, -1);
+    document.getElementById("input").value = q;
+}
+ 
+// This function display values
+function display(value) {
+    document.getElementById("input").value += value;
+}
+ 
+// This functioĺp0n evaluates the expression and returns result
+function calculate() {
+    var p = document.getElementById("input").value;
+    var q = eval(p);
+    document.getElementById("input").value = q;
+}
